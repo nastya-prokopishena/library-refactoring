bf87250c036c98382f0b32902597b787c8435b77
nastya-prokopishena <anastasiia.prokopishena@lnu.edu.ua>
2025-05-19 16:55:28 +0300

    update Docker and GitHub Actions

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-    FLASK_ENV = os.getenv("FLASK_ENV", "production")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/library_db")
+    FLASK_ENV = os.getenv("FLASK_ENV", "development")
